f4d87906d29ec27d1bb18844e19e2c8a8afd122e
www-data <www-data@s5316065.elf.ict.griffith.edu.au>
2023-09-01 19:51:07 +1000

    Add foreach to display posts title in home and change route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,12 @@
 
 @section('content')
     <h1>Welcome to Assignment1</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li>
+                <a href="#">{{ $post->title }}</a> by {{ $post->author }}
+            </li>
+        @endforeach
+    </ul>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = DB::select('SELECT * FROM posts ORDER BY date DESC');
+    return view('home', ['posts' => $posts]);
 });
