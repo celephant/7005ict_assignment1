c6e5857856ac36586cd3fc2d367af2263018052e
www-data <www-data@s5316065.elf.ict.griffith.edu.au>
2023-09-01 22:47:06 +1000

    Added form for creating new posts on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,26 @@
 @section('title','Home')
 
 @section('content')
+    <h1>Home Page</h1>
+
+    <!-- Form to Create New Post -->
+    <h2>Create a New Post</h2>
+    <form action="/create-post" method="post">
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required><br>
+
+        <label for="author">Author:</label>
+        <input type="text" id="author" name="author" required><br>
+
+        <label for="message">Message:</label>
+        <input id="message" name="message" required><br>
+
+        <input type="submit" value="Create Post">
+    </form>
+
+    <!-- List of Posts -->
+    <h2>Posts</h2>
     <ul>
         @foreach($posts as $post)
             <li>
@@ -10,5 +30,4 @@
             </li>
         @endforeach
     </ul>
-
 @endsection
