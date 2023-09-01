b284c7864ad89da00f4a5be1dcbdd0b0b15268a6
www-data <www-data@s5316065.elf.ict.griffith.edu.au>
2023-09-01 11:12:59 +1000

    Add initial data

--- database/database1.sql
+++ database/database1.sql
@@ -1,3 +1,8 @@
+-- drop table before create
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+
+
 -- create user table
 CREATE TABLE users (
     id INTEGER,
@@ -13,7 +18,14 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     message TEXT NOT NULL,
-    date TEXT NOT NULL,
+    date NUMERIC NOT NULL,
     PRIMARY KEY(id)
 );
 
+
+-- insert initial data
+INSERT INTO users (id, name, email, password) VALUES (1, 'Ze Zeng', 'ze@example.com', 'password');
+INSERT INTO users (id, name, email, password) VALUES (2, 'TEST ONE', 'testone@example.com', 'testone');
+
+INSERT INTO posts (id, title, author, message, date) VALUES (1, 'initial post', 'Ze Zeng', 'This is my 7005ict assingment1.', '31-1-2000');
+INSERT INTO posts (id, title, author, message, date) VALUES (2, 'test title', 'TEST ONE', 'My name is TEST ONE.', '12-12-1960');
