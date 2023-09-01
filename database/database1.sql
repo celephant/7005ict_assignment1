4fe903b4fd7c2ebadc58b687a0559a96329d9f3c
www-data <www-data@s5316065.elf.ict.griffith.edu.au>
2023-09-02 00:18:14 +1000

    post in chronological order

--- database/database1.sql
+++ database/database1.sql
@@ -29,7 +29,7 @@ INSERT INTO users (id, name, email, password) VALUES (2, 'Test Two', 'testtwo@ex
 INSERT INTO users (id, name, email, password) VALUES (3, 'Test Three', 'testthree@example.com', 'testthree');
 INSERT INTO users (id, name, email, password) VALUES (4, 'Test Four', 'testfour@example.com', 'testfour');
 
-INSERT INTO posts (id, title, author, message, date) VALUES (1, 'initial post', 'Ze Zeng', 'This is my 7005ict assingment1.', '31-1-2023');
-INSERT INTO posts (id, title, author, message, date) VALUES (2, 'test title', 'Test Three', 'My name is TEST ONE.', '11-1-2000');
-INSERT INTO posts (id, title, author, message, date) VALUES (3, 'this is post title', 'Test Two', 'This is my 7005ict assingment1 , post title.', '31-5-2000');
-INSERT INTO posts (id, title, author, message, date) VALUES (4, 'this is link go to comments detail', 'Test Three', 'this is my second posts.', '12-12-2002');
+INSERT INTO posts (id, title, author, message, date) VALUES (1, 'initial post', 'Ze Zeng', 'This is my 7005ict assingment1.', '01/01/2023');
+INSERT INTO posts (id, title, author, message, date) VALUES (2, 'test title', 'Test Three', 'My name is TEST ONE.', '01/02/2023');
+INSERT INTO posts (id, title, author, message, date) VALUES (3, 'this is post title', 'Test Two', 'This is my 7005ict assingment1 , post title.', '01/03/2023');
+INSERT INTO posts (id, title, author, message, date) VALUES (4, 'this is link go to comments detail', 'Test Three', 'this is my second posts.', '01/04/2023');
